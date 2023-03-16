658f6ec05fbcd879b308240a51ecc4dda360a63f
unknown <colantoniobruno166@gmail.com>
2023-03-16 14:53:48 -0300

    Ejercicio1_Clase7

---
+++ Clase7/Ej1.py
@@ -0,0 +1,13 @@
+import calculatorModule as calculadora
+
+#operaciones
+suma = calculadora.sumar(5, 4)
+resta = calculadora.restar(9, 7)
+multiplicacion = calculadora.multiplicar(8, 5)
+division = calculadora.dividir(6, 3)
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+
